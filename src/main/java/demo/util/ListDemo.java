1d551688d5bb1e4ddc0d135d805483d8c13784ca
lostsquirrel <songli1988@gmail.com>
2016-11-01 14:24:15 +0800

    test list sort with comparator

--- src/main/java/demo/util/ListDemo.java
+++ src/main/java/demo/util/ListDemo.java
@@ -1,7 +1,9 @@
 package demo.util;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -28,5 +30,25 @@ public class ListDemo {
 		for (String item : list) {
 			log.debug(item);
 		}
+	}
+	
+	@Test
+	public void testSort() throws Exception {
+		List<Integer> list = new ArrayList<>();
+		Random r = new Random();
+		for (int i = 0; i < 10; i++) {
+			list.add(r.nextInt());
+		}
+		log.debug(list.toString());
+		list.sort(new Comparator<Integer>(){
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				int c = o1.compareTo(o2);
+				log.debug("{} ? {} : {}", o1, o2, c);
+				return c;
+			}
+		});
+		log.debug(list.toString());
 	}
 }

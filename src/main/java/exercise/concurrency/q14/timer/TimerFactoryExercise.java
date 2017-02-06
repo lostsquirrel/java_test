8a4faa5b70e7a98672ac3ab1a693844584a4cda2
lostsquirrel <songli1988@gmail.com>
2017-02-06 13:30:49 +0800

    add description to exercise 14

--- src/main/java/exercise/concurrency/q14/timer/TimerFactoryExercise.java
+++ src/main/java/exercise/concurrency/q14/timer/TimerFactoryExercise.java
@@ -3,6 +3,11 @@ package exercise.concurrency.q14.timer;
 
 public class TimerFactoryExercise {
 	
+	/**
+	 * 在定时时间到了以后，执行指定任务
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		TimerFactory.createTimer(new MyTimerTask());
 		for (int i = 0; i < 500; i++) {

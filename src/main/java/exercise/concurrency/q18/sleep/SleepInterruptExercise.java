231e75bed1bebc6a017fbc9a007d2cbac3f27ae0
lostsquirrel <songli1988@gmail.com>
2017-02-09 10:22:20 +0800

    ent

--- src/main/java/exercise/concurrency/q18/sleep/SleepInterruptExercise.java
+++ src/main/java/exercise/concurrency/q18/sleep/SleepInterruptExercise.java
@@ -6,6 +6,11 @@ import java.util.concurrent.TimeUnit;
 
 public class SleepInterruptExercise {
 
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 * 验证sleep的线程能被打断
+	 */
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService exec = Executors.newCachedThreadPool();
 		

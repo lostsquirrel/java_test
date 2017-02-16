272cd1bdf62e1df6c3bb80d3319b03c49cabdd24
lostsquirrel <songli1988@gmail.com>
2017-02-16 14:26:40 +0800

    add queues demo

---
+++ src/main/java/demo/concurrency/queue/v2/ToastQueue.java
@@ -0,0 +1,9 @@
+package demo.concurrency.queue.v2;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ToastQueue extends LinkedBlockingQueue<Toast> {
+
+	private static final long serialVersionUID = 1L;
+
+}

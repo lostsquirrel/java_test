eb7b2ac2b1f86a1cd44b3daea295a5323ed1e8d5
lostsquirrel <songli1988@gmail.com>
2018-03-01 07:27:44 +0800

    add daemon thread demo

---
+++ concurrency-thread/src/main/java/demo/concurrency02/thread/daemon/DaemonDemo.java
@@ -0,0 +1,22 @@
+package demo.concurrency02.thread.daemon;
+
+import demo.concurrency.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 当一个Java应用内，只有守护线程时，Java虚拟机就会自然退出
+ * 当主线程结束后，程序结束, 且数字打印还未到设置的值
+ */
+public class DaemonDemo {
+    private static final Logger log = LoggerFactory.getLogger(DaemonDemo.class);
+
+    public static void main(String[] args) {
+
+        Thread t = new Thread(()-> Utils.busyBoy(Integer.MAX_VALUE), "daemon thread demo");
+        t.setDaemon(true);
+        t.start();
+        Utils.doWork(Utils.TIMEOUT_3S);
+        log.debug("main thread finished");
+    }
+}

076e943bf42d1ef67ac236f552b01a487d2db027
lostsquirrel <songli1988@gmail.com>
2018-03-07 09:05:37 +0800

    change priority demo output to sto

--- concurrency-thread/src/main/java/demo/concurrency03/thread/priority/PriorityDemo.java
+++ concurrency-thread/src/main/java/demo/concurrency03/thread/priority/PriorityDemo.java
@@ -6,6 +6,8 @@ import demo.concurrency.Utils;
  * 高优先级的线程更容易再竞争中获胜
  * 优先级，分别为 高 普通 低的 依次结束
  * 使用 单核 机器运行
+ *  docker run -it --rm --cpus=1 registry.cn-hangzhou.aliyuncs.com/lisong/java:8_server-jre cat /proc/cpuinfo
+ *  docker run -it --rm --cpuset-cpus="0" -v $(pwd)/target:/app registry.cn-hangzhou.aliyuncs.com/lisong/java:8_server-jre java -cp /app/java_test-0.0.1-SNAPSHOT-jar-with-dependencies.jar demo.concurrency03.thread.priority.PriorityDemo
  */
 public class PriorityDemo {
     public static void main(String[] args) {
@@ -13,7 +15,13 @@ public class PriorityDemo {
         String[] pns = new String[]{"MAX_PRIORITY", "NORM_PRIORITY", "MIN_PRIORITY"};
         int x = 0;
         for (int p : ps) {
-            Thread t = new Thread(()->Utils.busyBoy(Short.MAX_VALUE), pns[x]);
+            Thread t = new Thread(()->{
+                int y = 0;
+                for (int i = 0; i < Short.MAX_VALUE; i++) {
+                    y += i;
+                }
+                System.out.println(String.format("Thread %s finished", Thread.currentThread().getName()));
+            }, pns[x]);
             x++;
             t.setPriority(p);
             t.start();

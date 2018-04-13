11bfe20f3914637a656e18e9645418462209dde8
lostsquirrel <songli1988@gmail.com>
2018-04-13 17:13:08 +0800

    init project

---
+++ java8-date/src/main/java/demo/java/hello/Hello.java
@@ -0,0 +1,13 @@
+package demo.java.hello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+       System.out.println("hello java");
+    }
+}
+
